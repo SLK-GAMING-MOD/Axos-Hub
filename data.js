d7626466d0a7a5a7249ecc80993e81c85562c43b
SLK-GAMING-MOD <htzgamingssr@gmail.com>
2026-08-31 18:12:18 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -3,37 +3,36 @@
 //      (vd: "delta.png") nếu ảnh đã bỏ sẵn trong folder "Image" của repo
 const scriptsData = [
     {
-        title: "Delta x General",
+        title: "Delta x General v2.735",
         image: "1788172132333.jpg",
         description: "Bản executor dành cho mobile, hỗ trợ anti ban siêu mạnh.",
         primaryBtn: {
-            label: "Download Apk",
+            label: "Tải Apk",
             link: "https://link4m.net/MmhCX3g"
         },
         secondaryBtn: null
     },
     {
-        title: "ESP Framework Trực Quan",
-        image: "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?q=80&w=800&auto=format&fit=crop",
-        description: "Module ESP hoàn chỉnh sử dụng Tracers, 2D Bounding Boxes và Highlights. Hỗ trợ track Survivors và Killers mượt mà.",
+        title: "Delta x VNG v2.735",
+        image: "1788172132333.jpg",
+        description: "Bản executor dành cho mobile, hỗ trợ anti ban siêu mạnh.",
         primaryBtn: {
-            label: "Xem Source",
-            link: "#"
+            label: "Tải Apk",
+            link: "https://link4m.org/DUno9I"
         },
         secondaryBtn: null
     },
     {
-        title: "WindUI Custom Theme",
-        image: "https://images.unsplash.com/photo-1629654291663-b91ad427698f?q=80&w=800&auto=format&fit=crop",
-        description: "Bộ giao diện tuỳ biến bằng WindUI library. Chỉnh sửa ColorPickers, Button Gradients và Layout chuẩn.",
+        title: "Delta x VNG v2.735 (Login)",
+        image: "1788172132333.jpg",
+        description: "Bản executor dành cho mobile, hỗ trợ anti ban siêu mạnh.",
         primaryBtn: {
-            label: "Mở GitHub",
-            link: "#"
+            label: "Tải Apk 1",
+            link: "https://link4m.net/YBPsU2q3"
         },
         secondaryBtn: {
-            label: "Xem Hướng Dẫn",
-            action: "link",
-            data: "#"
+            label: "Tải Apk 2",
+            link: "https://link4m.org/ZETQV"
         }
     }
 ];
